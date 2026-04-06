04c89d99a3cf4c80a63b659600e996d6a6206726
Devil1416 <itsharsh1416@gmail.com>
2026-04-06 12:32:55 +0530

    upgrade: add runnable project

--- src/planner.py
+++ src/planner.py
@@ -1,13 +1,11 @@
-from .graph import graph
-def solve(start):
+graph = {'start': {'align': 2, 'wait': 5}, 'align': {'burn': 3}, 'burn': {'orbit': 4}, 'wait': {'orbit': 10}, 'orbit': {}}
+
+def run():
     dist = {n:999 for n in graph}
-    dist[start]=0
+    dist['start'] = 0
     for _ in graph:
         for u in graph:
             for v,w in graph[u].items():
-                if dist[u]+w < dist[v]:
-                    dist[v]=dist[u]+w
-    return dist
-
-def run():
-    print(solve("start"))
+                if dist[u] + w < dist[v]:
+                    dist[v] = dist[u] + w
+    print(dist)
